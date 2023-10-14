a0bf8c163621d1ebb9cc045f35116e9ad4bd2810
Nando Sola <nando@robotchrist.com>
2023-10-14 02:01:20 +0200

    deploy: add cloudwatch logs for api_gw

--- deploy/api_gateway.tf
+++ deploy/api_gateway.tf
@@ -9,8 +9,13 @@ module "api_gateway" {
   domain_name                 = "${local.subdomain}.${local.domain_name}"
   domain_name_certificate_arn = module.acm.acm_certificate_arn
 
+  default_stage_access_log_destination_arn = aws_cloudwatch_log_group.api_gw.arn
+  default_stage_access_log_format          = "$context.identity.sourceIp - - [$context.requestTime] \"$context.httpMethod $context.routeKey $context.protocol\" $context.status $context.responseLength $context.requestId $context.integrationErrorMessage"
+
+
   integrations = {
     "GET /hello/{username}" = {
+      integration_type       = "AWS_PROXY"
       lambda_arn             = module.lambda_get.lambda_function_arn
       payload_format_version = "2.0"
     }
@@ -20,3 +25,10 @@ module "api_gateway" {
     }
   }
 }
+
+resource "aws_cloudwatch_log_group" "api_gw" {
+  name = "/aws/apigw/${random_pet.this.id}-http"
+
+  retention_in_days = 30
+}
+
